0736b577e5d6f9bb1ced252aa6ee21dea025fc75
BuildTools <emiliano.lopez@est.fi.uncoma.edu.ar>
2022-10-31 21:11:51 -0300

    arreglo 2.0
    vamaaa

--- programaCamussoGonzalezLopezRossi.php
+++ programaCamussoGonzalezLopezRossi.php
@@ -6,17 +6,11 @@ include_once("wordix.php");
 /**************************************/
 
 /* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
-/* Gonzalez Lautaro - FAI-2950 - TUDW - lautarofglez@gmail.com - LautaroGonzalez-FAI2950 */
-/* ... COMPLETAR ... */
-/*Camusso Valentin - FAI-3208 - TUDW - valentin.camusso@est.fi.uncoma.edu.ar - github.com/ValentinCamussoFAI-3208 */
-/*Rossi Julia - FAI-2378 - TUDW - Mail: julia.rossi@est.fi.uncoma.edu.ar - Github: JuliaRossiFAI-2378 */
-/* Emiliano Lopez - FAI-3357 - Carrera: TUDW - Mail: emiliano.lopez@est.fi.uncoma.edu.ar - Github: EmiMlz
-=======
-/* Gonzalez Lautaro FAI-2950 Carrera: TUDW lautarofglez@gmail.com LautaroGonzalez-FAI2950
-Camusso Valentin - FAI-3208 - TUDW - valentin.camusso@est.fi.uncoma.edu.ar - github.com/ValentinCamussoFAI-3208
+/* Gonzalez Lautaro FAI-2950 - Carrera: TUDW lautarofglez@gmail.com LautaroGonzalez-FAI2950
+Camusso Valentin - FAI-3208 - Carrera: TUDW - valentin.camusso@est.fi.uncoma.edu.ar - github.com/ValentinCamussoFAI-3208
 Emiliano Lopez - FAI-3357 - Carrera: TUDW - Mail: emiliano.lopez@est.fi.uncoma.edu.ar - Github: EmiMlz 
-Rossi Julia - FAI-2378 - TUDW - Mail: julia.rossi@est.fi.uncoma.edu.ar - Github: JuliaRossiFAI-2378 */`
-
+Rossi Julia - FAI-2378 - Carrera: TUDW - Mail: julia.rossi@est.fi.uncoma.edu.ar - Github: JuliaRossiFAI-2378 */`
+`
 /**************************************/
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
