f65700d120690c19893365e82860ce363977ae1c
LautaroGonzalez-FAI2950 <lautarofglez@gmail.com>
2022-11-05 16:39:14 -0300

    Cambio en la linea 119

--- wordix.php
+++ wordix.php
@@ -116,6 +116,7 @@ function escribirSegunEstado($texto, $estado)
 
 /**
  * Escribe por pantalla un mensaje de bienvenida al usuario que está jugando
+ * //COMPLETADO
  */
 function escribirMensajeBienvenida($usuario)
 {
