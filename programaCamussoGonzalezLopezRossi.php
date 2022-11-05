b8d1cb9b652dce9d440c6ca44cceedc6a8dd40de
BuildTools <emiliano.lopez@est.fi.uncoma.edu.ar>
2022-11-05 18:00:31 -0300

    Agregue la funcion cargarColeccionPartidas
    Complete el arreglo $coleccionPartidas y rellene los datos verificado con la funcion obtenerPuntajeWordix

--- programaCamussoGonzalezLopezRossi.php
+++ programaCamussoGonzalezLopezRossi.php
@@ -31,8 +31,31 @@ function cargarColeccionPalabras()
 
     return ($coleccionPalabras);
 }
+/**
+ * Obtiene una coleccion de partidas
+ * @return array
+ */
+function cargarColeccionPartidas()
+{
+    $coleccionPartidas = [];
+    $coleccionPartidas [0]= ["palabraWordix" => "QUESO","jugador" => "majo","intentos" => 0,"puntaje" => 0];
+    $coleccionPartidas [1]= ["palabraWordix" => "CASAS","jugador" => "rudolf","intentos" => 3,"puntaje" => 14];
+    $coleccionPartidas [2]= ["palabraWordix" => "QUESO","jugador" => "pink2000","intentos" => 6,"puntaje" => 10];
+    $coleccionPartidas [3]= ["palabraWordix" => "FUEGO","jugador" => "noobmaster69","intentos" => 1,"puntaje" => 13];
+    $coleccionPartidas [4]= ["palabraWordix" => "TINTO","jugador" => "jebus","intentos" => 1,"puntaje" => 17];
+    $coleccionPartidas [5]= ["palabraWordix" => "HUEVO","jugador" => "julia","intentos" => 4,"puntaje" => 11];
+    $coleccionPartidas [6]= ["palabraWordix" => "LOSAS","jugador" => "felixmodelorjr","intentos" => 2,"puntaje" => 15];
+    $coleccionPartidas [7]= ["palabraWordix" => "RATON","jugador" => "mickey","intentos" => 5,"puntaje" => 13];
+    $coleccionPartidas [8]= ["palabraWordix" => "GATOS","jugador" => "majo","intentos" => 2,"puntaje" => 15];
+    $coleccionPartidas [9]= ["palabraWordix" => "NAVES","jugador" => "buzzlightyear","intentos" => 1,"puntaje" => 17];
 
-/* ... COMPLETAR ... */
+
+
+    //COMPLETADO
+    ;
+
+    return ($coleccionPartidas);
+}
 
 
 
